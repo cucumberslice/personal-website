acf578a43663dd775526cb1af5186ceb04011e3a
Yeshi Wangdi <ywblackberry@gmail.com>
2018-09-05 00:15:27 -0400

    added h2,p in contact and css

--- myportfoliosite/src/components/contact.js
+++ myportfoliosite/src/components/contact.js
@@ -6,7 +6,21 @@ class Contact extends Component {
         return (
             <div className='contact-body'>
                 <Grid className='contact-grid'>
-                    <Cell col={6}>half page</Cell>
+                    <Cell col={6}>
+
+                        <h2>Yeshi Wangdi</h2>
+                        <img 
+                            src='https://previews.123rf.com/images/marish/marish1801/marish180100014/93241128-cute-cat-illustration-icon-avatar.jpg'
+                            alt='avatar'
+                            style={{height: '250px'}}
+                        />
+                        <p style={{width: '75%', margin: 'auto', paddingTop: '1em' }}>
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's
+                         standard dummy text ever since the 1500s, 
+                        when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries
+                        </p>
+
+                    </Cell>
                     <Cell col={6}>half page</Cell>
                 </Grid>
             </div>
