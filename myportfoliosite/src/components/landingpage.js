fd110448c967b13425d4132eb17fdc758b404679
Yeshi Wangdi <ywblackberry@gmail.com>
2018-09-04 23:54:38 -0400

    projects component is done

--- myportfoliosite/src/components/landingpage.js
+++ myportfoliosite/src/components/landingpage.js
@@ -1,10 +1,44 @@
 import React, { Component } from 'react'
+import {Grid,Cell} from 'react-mdl'
 
 class LandingPage extends Component {
     render() {
         return (
-            <div>
-                <h1>LandingPage</h1>
+            <div style={{width: '100', margin:'auto'}}>
+                <Grid className='landing-grid'>
+                    <Cell col={12}>
+                        <img src='https://cdn1.iconfinder.com/data/icons/user-pictures/101/malecostume-512.png' alt='avatar' className='avatar-img'/>
+                        <div className='banner-text'>
+                            <h1>Full Stack Web Developer</h1>
+
+                            <hr />
+
+                            <p>HTML/CSS | Boostrap | Javascript | React | React Native | NodeJS | Express</p>
+
+                            <div className='social-links'>
+                                {/* LinkedIn */}
+                                <a href='http://google.com' target='_blank' rel='noonpener noreferrer' >
+                                    <i className='fa fa-linkedin-square' aria-hidden='true' />
+                                </a>
+
+                                 {/* Github */}
+                                 <a href='http://google.com' target='_blank' rel='noonpener noreferrer' >
+                                    <i className='fa fa-github-square' aria-hidden='true' />
+                                </a>
+
+                                 {/* Freecodecamp */}
+                                 <a href='http://google.com' target='_blank' rel='noonpener noreferrer' >
+                                    <i className='fa fa-free-code-camp' aria-hidden='true' />
+                                </a>
+
+                                 {/* Youtube  */}
+                                 <a href='http://google.com' target='_blank' rel='noonpener noreferrer' >
+                                    <i className='fa fa-youtube-square' aria-hidden='true' />
+                                </a>
+                            </div>
+                        </div>
+                    </Cell>
+                </Grid>
             </div>
         )
     }
