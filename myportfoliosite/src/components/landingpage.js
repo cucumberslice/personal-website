ba3cdd786e3c7117b6f9a20b78baa1f510df0ba8
Yeshi Wangdi <ywblackberry@gmail.com>
2018-09-19 17:43:43 -0400

    added linkedin

--- myportfoliosite/src/components/landingpage.js
+++ myportfoliosite/src/components/landingpage.js
@@ -3,44 +3,45 @@ import {Grid,Cell} from 'react-mdl'
 
 class LandingPage extends Component {
     render() {
-        return (
-            <div style={{width: '100', margin:'auto'}}>
-                <Grid className='landing-grid'>
-                    <Cell col={12}>
-                        <img src='https://cdn1.iconfinder.com/data/icons/user-pictures/101/malecostume-512.png' alt='avatar' className='avatar-img'/>
-                        <div className='banner-text'>
-                            <h1>Full Stack Web Developer</h1>
+        return <div style={{ width: "100", margin: "auto" }}>
+            <Grid className="landing-grid">
+              <Cell col={12}>
+                <img src="https://cdn1.iconfinder.com/data/icons/user-pictures/101/malecostume-512.png" alt="avatar" className="avatar-img" />
+                <div className="banner-text">
+                  <h1>Full Stack Web Developer</h1>
 
-                            <hr />
+                  <hr />
 
-                            <p>HTML/CSS | Boostrap | Javascript | React | React Native | NodeJS | Express | Postgres SQL</p>
+                  <p>
+                    HTML/CSS | Boostrap | Javascript | React | React
+                    Native | NodeJS | Express | Postgres SQL
+                  </p>
 
-                            <div className='social-links'>
-                                {/* LinkedIn */}
-                                <a href='http://google.com' target='_blank' rel='noonpener noreferrer' >
-                                    <i className='fa fa-linkedin-square' aria-hidden='true' />
-                                </a>
+                  <div className="social-links">
+                    {/* LinkedIn */}
+                    <a href="https://www.linkedin.com/in/yeshi-wangdi/" target="_blank" rel="noonpener noreferrer">
+                      <i className="fa fa-linkedin-square" aria-hidden="true" />
+                    </a>
 
-                                 {/* Github */}
-                                 <a href='http://google.com' target='_blank' rel='noonpener noreferrer' >
-                                    <i className='fa fa-github-square' aria-hidden='true' />
-                                </a>
+                    {/* Github */}
+                    <a href="http://google.com" target="_blank" rel="noonpener noreferrer">
+                      <i className="fa fa-github-square" aria-hidden="true" />
+                    </a>
 
-                                 {/* Freecodecamp */}
-                                 <a href='http://google.com' target='_blank' rel='noonpener noreferrer' >
-                                    <i className='fa fa-free-code-camp' aria-hidden='true' />
-                                </a>
+                    {/* Freecodecamp */}
+                    <a href="http://google.com" target="_blank" rel="noonpener noreferrer">
+                      <i className="fa fa-free-code-camp" aria-hidden="true" />
+                    </a>
 
-                                 {/* Youtube  */}
-                                 <a href='http://google.com' target='_blank' rel='noonpener noreferrer' >
-                                    <i className='fa fa-youtube-square' aria-hidden='true' />
-                                </a>
-                            </div>
-                        </div>
-                    </Cell>
-                </Grid>
-            </div>
-        )
+                    {/* Youtube  */}
+                    <a href="http://google.com" target="_blank" rel="noonpener noreferrer">
+                      <i className="fa fa-youtube-square" aria-hidden="true" />
+                    </a>
+                  </div>
+                </div>
+              </Cell>
+            </Grid>
+          </div>;
     }
 }
 
